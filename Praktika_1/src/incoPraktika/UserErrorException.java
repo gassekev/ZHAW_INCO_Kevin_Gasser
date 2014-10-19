4eea1fd56b59f75de1eb7bd59a3f5b3c211f0a54
gassekev <gassekev@students.zhaw.ch>
2014-10-19 19:03:38 +0200

    adding inco praktika 1

---
+++ Praktika_1/src/incoPraktika/UserErrorException.java
@@ -0,0 +1,13 @@
+package incoPraktika;
+
+public class UserErrorException extends Exception {
+
+	private static final long serialVersionUID = 42L;
+	
+	public UserErrorException( String msg) {
+		super( msg);
+	} 
+	
+}
+
+
